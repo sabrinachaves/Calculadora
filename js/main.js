4caaf76985c1e15bef9f52e9a95f53459343df75
sabrinachs <sabrinach08@yahoo.com.br>
2022-06-20 08:32:55 -0300

    Alterando index

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 let display = document.querySelector('.display');
 const keyList = document.querySelectorAll('.number');
 const signList = document.querySelectorAll('.sign');
+let expression = document.querySelector('.expression');
 let resultado;
 let num1;
 let num2;
 let operator;
+let tempOperator;
 let count = 0;
 
 for(let indice=0; indice < keyList.length;indice++){
@@ -12,22 +14,29 @@ for(let indice=0; indice < keyList.length;indice++){
     
     key.onclick = function(){
 
-        if(key.value == "CE"){
+        if(key.value == "C"){
             deleteDisplay();
+            deleteExpression();
         }else if(key.value == "="){
             num2 = parseFloat(display.value);
+            expression.innerHTML += num2 + "=";
             display.value = calculate(num1, num2, operator);
         }else if(key.value == "+" || key.value == "-" || key.value == "/" || key.value == "*"){
+            count++;
+            if(count > 1){
+                num2 = parseFloat(display.value);
+                display.value = parseFloat(calculate(num1, num2, operator));
+            }
             num1 = parseFloat(display.value);
             operator = key.value;
-            deleteDisplay();
-            display.value = key.value;
-            count++;
+            tempOperator = operator;
+            expression.innerHTML = num1+operator;
         }
         else{
-            if(display.value == "+" || display.value == "-" || display.value == "/" || display.value == "*"){
+            if(tempOperator == "+" || tempOperator == "-" || tempOperator == "/" || tempOperator == "*"){
                 deleteDisplay();
-            } 
+                tempOperator = "";
+            }
             display.value = display.value + key.value;
         }
     }
@@ -35,6 +44,10 @@ for(let indice=0; indice < keyList.length;indice++){
 
 function deleteDisplay(){
     display.value = "";
+}
+
+function deleteExpression(){
+    expression.innerHTML = "";
 }
 
 function calculate(num1, num2, operator){
